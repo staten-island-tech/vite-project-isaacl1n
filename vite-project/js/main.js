6f1a13ff1d489206a0e61a541426c46ceff0c187
isaacl81 <isaaclin2007@gmail.com>
2023-11-17 18:46:03 -0500

    body styled

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,22 +3,18 @@ import javascriptLogo from '../javascript.svg'
 import viteLogo from '../public/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const DOMselectors = {
+    body: document.body,
+    toggleButton: document.getElementById("toggle-button"),
+}
 
-setupCounter(document.querySelector('#counter'))
+function toggleColors() {
+    if (DOMselectors.body.classList.contains("light")) {
+        DOMselectors.body.classList.remove("light");
+        DOMselectors.body.classList.add("dark");
+    } else if (DOMselectors.body.classList.contains("dark")) {
+        DOMselectors.body.classList.remove("dark");
+        DOMselectors.body.classList.add("light");
+    };
+};
+DOMselectors.toggleButton.addEventListener("click", toggleColors);
